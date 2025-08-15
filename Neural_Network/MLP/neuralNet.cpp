644d3c80684231749b8d0114cbbe69b6a7bcbc40
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-15 13:58:44 +0545

    docs: NN Started

--- Neural_Network/MLP/neuralNet.cpp
+++ Neural_Network/MLP/neuralNet.cpp
@@ -5,5 +5,13 @@ using namespace std;
 
 int main()
 {
-  
+  Perceptron p(2); // Object with 2 inputs on the Stack, No need to delete
+
+  p.set_weights({10, 10, -15}); // +1 Bias
+
+  cout << "Gate: AND" << endl;
+  cout << "0 AND 0 = " << p.run({0, 0}) << endl;
+  cout << "0 AND 1 = " << p.run({0, 1}) << endl;
+  cout << "1 AND 0 = " << p.run({1, 0}) << endl;
+  cout << "1 AND 1 = " << p.run({1, 1}) << endl;
 }
