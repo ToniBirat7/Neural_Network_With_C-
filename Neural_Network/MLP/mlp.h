d374e480c25df380b23b005cf38129ff98751625
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-15 13:02:53 +0545

    docs: NN Started

--- Neural_Network/MLP/mlp.h
+++ Neural_Network/MLP/mlp.h
@@ -17,9 +17,12 @@ public:
   // Constructor
   Perceptron(size_t inputs, double bias = 1.0);
 
+  // Run the Perceptron
   double run(std::vector<double> x);
 
+  // Set the Customize Weights if Needed
   void set_weights(std::vector<double> w_init);
 
+  // Sigmoid Activation Function
   double sigmoid(double x);
 };
