e8e655e8f31a4328aab8eac2d45d7de223feb735
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-16 10:56:42 +0545

    docs: day 2

--- Neural_Network/MLP/neuralNet.cpp
+++ Neural_Network/MLP/neuralNet.cpp
@@ -5,13 +5,25 @@ using namespace std;
 
 int main()
 {
-  Perceptron p(2); // Object with 2 inputs on the Stack, No need to delete
+  // AND Gate
+  Perceptron andGate(2); // Object with 2 inputs on the Stack, No need to delete
 
-  p.set_weights({10, 10, -15}); // +1 Bias
+  andGate.set_weights({10, 10, -15}); // +1 Bias
 
   cout << "Gate: AND" << endl;
-  cout << "0 AND 0 = " << p.run({0, 0}) << endl;
-  cout << "0 AND 1 = " << p.run({0, 1}) << endl;
-  cout << "1 AND 0 = " << p.run({1, 0}) << endl;
-  cout << "1 AND 1 = " << p.run({1, 1}) << endl;
+  cout << "0 AND 0 = " << andGate.run({0, 0}) << endl;
+  cout << "0 AND 1 = " << andGate.run({0, 1}) << endl;
+  cout << "1 AND 0 = " << andGate.run({1, 0}) << endl;
+  cout << "1 AND 1 = " << andGate.run({1, 1}) << endl;
+
+  // OR Gate
+  Perceptron orGate(2); // Object with 2 inputs on the Stack, No need to delete
+
+  orGate.set_weights({15, 15, -10}); // +1 Bias
+
+  cout << "Gate: AND" << endl;
+  cout << "0 AND 0 = " << orGate.run({0, 0}) << endl;
+  cout << "0 AND 1 = " << orGate.run({0, 1}) << endl;
+  cout << "1 AND 0 = " << orGate.run({1, 0}) << endl;
+  cout << "1 AND 1 = " << orGate.run({1, 1}) << endl;
 }
