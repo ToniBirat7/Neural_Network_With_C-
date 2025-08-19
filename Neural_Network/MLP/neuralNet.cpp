ecbce7e9bc473d9ae8620a9b480c2490a5a043d2
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-19 12:24:35 +0545

    docs: run method completed

--- Neural_Network/MLP/neuralNet.cpp
+++ Neural_Network/MLP/neuralNet.cpp
@@ -26,4 +26,28 @@ int main()
   cout << "0 AND 1 = " << orGate.run({0, 1}) << endl;
   cout << "1 AND 0 = " << orGate.run({1, 0}) << endl;
   cout << "1 AND 1 = " << orGate.run({1, 1}) << endl;
+
+  // XOR Gate
+  // Instaintiate with 2 inputs, 2 Perceptron in hidden and 1 Perceptron in the Output
+  MultiLayerPerceptron mlp({2, 2, 1});
+
+  // Set the Weights, NAND Gate, OR Gate and then AND Gate
+  mlp.set_weights(
+      {{{-10, -10, 15}, {15, 15, -10}},
+       {{10, 10, -15}}});
+
+  cout << endl;
+
+  // Print Weights
+  cout << "Hardcoded Weights:" << endl;
+  mlp.print_weights();
+
+  cout << endl;
+
+  // Run the Network
+  cout << "XOR: " << endl;
+  cout << "0 0 = " << mlp.run({0, 0})[0] << endl; // For 0 0 Input, Output should be  0
+  cout << "0 0 = " << mlp.run({0, 1})[0] << endl; // For 0 1 Input, Output should be  1
+  cout << "0 0 = " << mlp.run({1, 0})[0] << endl; // For 1 0 Input, Output should be  1
+  cout << "0 0 = " << mlp.run({1, 1})[0] << endl; // For 1 1 Input, Output should be  0
 }
