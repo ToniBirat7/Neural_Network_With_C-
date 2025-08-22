315c06deeb9063d3a1a8bcf4abc7a6c593cfdebf
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-22 18:55:39 +0545

    docs: backpropagation cont

--- Neural_Network/MLP/mlp.cpp
+++ Neural_Network/MLP/mlp.cpp
@@ -69,6 +69,9 @@ MultiLayerPerceptron::MultiLayerPerceptron(std::vector<size_t> layers, double bi
     // Add Vector of Values Filled with Zeros
     values.push_back(vector<double>(layers[i], 0.0)); // Output of Each Neuron Value set to Zero based on the number of Neurons in Each layer
 
+    // Initialize the error term for each neuron
+    d.push_back(vector<double>(layers[i], 0.0));
+
     // Add Vector of Neurons
     network.push_back(vector<Perceptron>()); // Creates a temporary empty std::vector<Perceptron> object and pushes it into 'network'. Without '()', we would only refer to the type name, not an object, causing a compiler error.
 
@@ -130,15 +133,54 @@ vector<double> MultiLayerPerceptron::run(vector<double> x)
 double MultiLayerPerceptron::bp(vector<double> x, vector<double> y)
 {
   // Step 1: Feed a sample to the Network
+  vector<double> outputs = run(x); // Get the Output of Last Neuron for Each Input i.e. ŷ
 
   // Step 2: Calculate MSE
   double MSE = 0.0;
+  vector<double> error;
 
-  // Step 3: Calculate the Output Error Terms
+  // Calculate the error for each output
+  for (size_t i = 0; i < y.size(); i++)
+  {
+    error.push_back(y[i] - outputs[i]); // ŷ - y
+    MSE += error[i] * error[i];         // (ŷ - y)^2 i.e. MSE for Each Sample
+  }
+  MSE /= layers.back(); // Average of MSE or Loss Function i.e. ((ŷ - y)^2) / N, here N is the number of Perceptron in the output layer
+
+  // Step 3: Calculate the Error for the Neurons in the Last Layer
+  for (size_t i = 0; i < outputs.size(); i++)
+  {
+    d.back()[i] = outputs[i] * (1 - outputs[i]) * (error[i]); // Delta for the Output Layer
+  }
 
   // Step 4: Calculate the error term of each unit on each layer
+  for (size_t i = network.size() - 2; i > 0; i--)
+  {
+    for (size_t h = 0; h < network[i].size(); h++)
+    {
+      double fwd_error = 0.0;
+      for (size_t k = 0; k < layers[i + 1]; k++)
+      {
+        ; // Fill in the blank
+      }; // Fill in the blank
+    }
+  }
 
   // Step 5 and 6: Calculate the deltas and update the weights
+  for (size_t i = 1; i < network.size(); i++)
+  {
+    for (size_t j = 0; j < layers[i]; j++)
+    {
+      for (size_t k = 0; k < layers[i - 1] + 1; k++)
+      {
+        // Fill in the blank
+      }
+    }
+  }
+
+  // Reutrn MSE
+
+  return MSE;
 }
 
 // Print the Weights
